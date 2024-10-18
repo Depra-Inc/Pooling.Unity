22620add4781e523bf0981453663c388e4a69eb8
n.melnikov <n.melnikov@depra.org>
2024-10-18 20:47:39 +0400

    [v0.0.1] First release

---
+++ Runtime/PoolSettings.cs
@@ -0,0 +1,18 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// © 2024 Nikolay Melnikov <n.melnikov@depra.org>
+
+using System;
+using Depra.Borrow;
+using UnityEngine;
+
+namespace Depra.Pooling
+{
+	[Serializable]
+	public sealed class PoolSettings
+	{
+		[field: SerializeField] public string Key { get; private set; }
+		[field: SerializeField] public int Capacity { get; private set; } = 10;
+		[field: SerializeField] public int MaxCapacity { get; private set; } = 100;
+		[field: SerializeField] public BorrowStrategy BorrowStrategy { get; private set; }
+	}
+}
---
+++ Runtime/UnityObjectPool.cs
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: Apache-2.0
+// © 2024 Nikolay Melnikov <n.melnikov@depra.org>
+
+using System;
+using System.Runtime.CompilerServices;
+using Depra.Pooling.Object;
+using UnityEngine;
+#if ENABLE_IL2CPP
+using Unity.IL2CPP.CompilerServices;
+#endif
+
+namespace Depra.Pooling
+{
+#if ENABLE_IL2CPP
+	[Il2CppSetOption(Option.NullChecks, false)]
+	[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+#endif
+	public sealed class UnityObjectPool<TPooled> : IDisposable, IPool<TPooled>, IPoolHandle<TPooled>
+		where TPooled : MonoBehaviour, IPooled
+	{
+		private readonly ObjectPool<TPooled> _objectPool;
+
+		public UnityObjectPool(TPooled prefab, PoolSettings settings)
+		{
+			var objectFactory = new Factory(settings.Key, prefab);
+			_objectPool = new ObjectPool<TPooled>(settings.BorrowStrategy, objectFactory,
+				settings.Key, settings.Capacity, settings.MaxCapacity);
+		}
+
+		public void Dispose() => _objectPool.Dispose();
+
+		public int CountAll => _objectPool.CountAll;
+		public int CountActive => _objectPool.CountActive;
+		public int CountPassive => _objectPool.CountPassive;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public TPooled Request() => _objectPool.Request();
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Release(TPooled obj) => _objectPool.Release(obj);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		IPooled IPool.RequestPooled() => Request();
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		void IPool.ReleasePooled(IPooled pooled) => Release((TPooled)pooled);
+
+		void IPoolHandle<TPooled>.ReturnInstanceToPool(PooledInstance<TPooled> instance)
+		{
+			IPoolHandle<TPooled> handle = _objectPool;
+			handle.ReturnInstanceToPool(instance);
+		}
+
+#if ENABLE_IL2CPP
+		[Il2CppSetOption(Option.NullChecks, false)]
+		[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+#endif
+		private sealed class Factory : IPooledObjectFactory<TPooled>
+		{
+			private readonly TPooled _original;
+			private readonly Transform _parent;
+
+			public Factory(string key, TPooled original)
+			{
+				_original = original;
+				_parent = new GameObject($"{key} Pool").transform;
+			}
+
+			TPooled IPooledObjectFactory<TPooled>.Create(object key) =>
+				UnityEngine.Object.Instantiate(_original);
+
+			void IPooledObjectFactory<TPooled>.Destroy(object key, TPooled instance) =>
+				UnityEngine.Object.Destroy(instance.gameObject);
+
+			void IPooledObjectFactory<TPooled>.OnEnable(object key, TPooled instance)
+			{
+				instance.transform.SetParent(null);
+				instance.gameObject.SetActive(true);
+			}
+
+			void IPooledObjectFactory<TPooled>.OnDisable(object key, TPooled instance)
+			{
+				instance.transform.SetParent(_parent);
+				instance.gameObject.SetActive(false);
+			}
+		}
+	}
+}
