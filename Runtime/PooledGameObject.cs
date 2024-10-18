60674d279205a93c59332f3e88d6b11d62fa5f43
n.melnikov <n.melnikov@depra.org>
2024-10-18 21:30:38 +0400

    [v0.0.2] Adds PooledGameObject

---
+++ Runtime/PooledGameObject.cs
@@ -0,0 +1,25 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// © 2024 Nikolay Melnikov <n.melnikov@depra.org>
+
+using UnityEngine;
+#if ENABLE_IL2CPP
+using Unity.IL2CPP.CompilerServices;
+#endif
+
+namespace Depra.Pooling
+{
+#if ENABLE_IL2CPP
+	[Il2CppSetOption(Option.NullChecks, false)]
+	[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+#endif
+	public class PooledGameObject : MonoBehaviour, IPooled
+	{
+		public virtual void OnPoolCreate(IPool pool) { }
+
+		public virtual void OnPoolGet() { }
+
+		public virtual void OnPoolSleep() { }
+
+		public virtual void OnPoolReuse() { }
+	}
+}
